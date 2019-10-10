b1ae09bba2664de24abc458c72f5a56588e64767
Rohan Gautam <rohan1gautam@gmail.com>
2019-10-10 21:40:21 +0800

    added dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,17 @@ import 'package:numberpicker/numberpicker.dart';
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:flare_flutter/flare_controls.dart';
 
-void main() => runApp(MyApp());
+//TODO: if move just one wheel, both hour and min should reflect the wheel values
+//TODO make it intuitive what the bottom number selector does.
+//TODO: add app icon
+// TODO: show time to sleep at with bright colors first
+// TODO: add info page
+//TODO : make time viewer responsive
+
+void main() async {
+  // await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);// making it potrait-only
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -16,22 +26,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(brightness: Brightness.dark),
+      home: MyHomePage(title: 'snoozy'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
