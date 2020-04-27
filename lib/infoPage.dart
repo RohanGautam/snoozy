6d5ca2c55d6c342e73a8f293a70b7d429427e58d
RohanGautam <rohan1gautam@gmail.com>
2020-04-27 18:54:23 +0800

    fix scrolling error

--- lib/infoPage.dart
+++ lib/infoPage.dart
@@ -39,26 +39,28 @@ class _InfoButtonState extends State<InfoButton> {
             padding: EdgeInsets.all(8),
             margin: EdgeInsets.all(8),
             height: deviceHeight / 3,
-            child: Column(
-              children: <Widget>[
-                Text(
-                  'Snoozy v1.0',
-                  style: TextStyle(
-                      fontSize: 25, color: Color.fromRGBO(143, 254, 221, 1)),
-                ),
-                Text('What is snoozy?',
-                    style:
-                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                Text(
-                    'Snoozy helps you calculate when to get up if you go to sleep now (click the dog!), or when to go to bed if you want to wake up at a certain time(use the time slider wheels!)'),
-                Text('How does it work?',
-                    style:
-                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                Text(
-                    'Snoozy suggests its timings based on human sleep cycles (1.5 hrs each) and time taken to fall asleep(14 mins on average).'),
-                Text(
-                    'Waking up in the middle of a sleep cycle makes you feel groggy and not rested. No more with snoozy!'),
-              ],
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    'Snoozy v1.0',
+                    style: TextStyle(
+                        fontSize: 25, color: Color.fromRGBO(143, 254, 221, 1)),
+                  ),
+                  Text('What is snoozy?',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  Text(
+                      'Snoozy helps you calculate when to get up if you go to sleep now (click the dog!), or when to go to bed if you want to wake up at a certain time(use the time slider wheels!)'),
+                  Text('How does it work?',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  Text(
+                      'Snoozy suggests its timings based on human sleep cycles (1.5 hrs each) and time taken to fall asleep(14 mins on average).'),
+                  Text(
+                      'Waking up in the middle of a sleep cycle makes you feel groggy and not rested. No more with snoozy!'),
+                ],
+              ),
             ),
           );
         });
